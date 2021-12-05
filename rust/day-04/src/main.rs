0abb6814d41e5b898d80f2469bad1de9571e9433
Chris Wallace <chris@bitcollider.io>
2021-12-05 00:21:34 +0000

    WIP: incomplete refactor
    Extremely incomplete beginning of a refactor to use nested Vecs instead of ndarray. Not sure it's worth revisiting all the logic. There are other solutions out there that feel more concise than this will end up, and time would be better spent further grokking ownership, mutability, and references than in algorithmic futsing.

--- rust/day-04/src/main.rs
+++ rust/day-04/src/main.rs
@@ -1,63 +1,113 @@
-use ndarray::{Array3, Axis};
+use ndarray::{Array3, ArrayBase, Axis, Dim, ViewRepr};
 use regex::Regex;
 
 const INPUT: &str = include_str!("../data/test.txt");
+const GRID_SIZE: usize = 5;
+
+type Cell = (bool, i32);
+type Board = Vec<Vec<Cell>>;
+
+fn check_rows(board: &Board) -> bool {
+    board
+        .iter()
+        .any(|row| row.iter().all(|&(marked, _)| marked))
+}
+
+fn check_columns(board: &Board) -> bool {
+    board
+        .iter()
+        .fold(vec![true, true, true, true, true], |acc, row| {
+            let row_marks: Vec<bool> = row.iter().map(|&(marked, _)| marked).collect();
+            row_marks
+                .iter()
+                .zip(acc.iter())
+                .map(|(&r, &a)| r & a)
+                .collect()
+        })
+        .iter()
+        .any(|&col| col)
+}
 
 fn get_scores(input: &str) -> Vec<i32> {
-    let mut split = input.splitn(2, "\n\n");
-    let numbers: Vec<&str> = split.next().unwrap().split(',').collect();
-    let grids: &str = split.next().unwrap();
+    let (draws_string, boards_string) = input.split_once("\n\n").unwrap();
 
-    let num: Regex = Regex::new(r"\d+").unwrap();
-    let flat: Vec<&str> = num.find_iter(grids).map(|x| x.as_str()).collect();
+    let draws: Vec<i32> = draws_string
+        .split(',')
+        .map(|draw| draw.parse::<i32>().unwrap())
+        .collect();
 
-    let grid_size = 5;
+    let mut boards: Vec<Board> = boards_string
+        .split("\n\n")
+        .map(|board| {
+            board
+                .split('\n')
+                .map(|row| {
+                    row.split_whitespace()
+                        .map(|cell| (false, cell.parse::<i32>().unwrap()))
+                        .collect()
+                })
+                .collect()
+        })
+        .collect();
 
-    let shape = (&flat.len() / (grid_size * grid_size), grid_size, grid_size);
+    println!("{:?}", boards);
 
-    let mut arr = Array3::from_shape_vec(shape, flat).unwrap();
-    let mut scores: Vec<i32> = Vec::new();
-
-    for n in numbers {
-        arr.mapv_inplace(|a| if a == n { "x" } else { a });
-
-        let winners = arr
-            .outer_iter()
-            .enumerate()
-            .filter(|(_, grid)| {
-                (grid
-                    .columns()
-                    .into_iter()
-                    .any(|col| col.iter().all(|&c| c == "x")))
-                    || (grid
-                        .rows()
-                        .into_iter()
-                        .any(|row| row.iter().all(|&r| r == "x")))
+    for draw in draws {
+        boards.iter_mut().map(|board| {
+            board.iter_mut().map(|row| {
+                row.iter_mut().for_each(|&mut (mut marked, value)| {
+                    if value == draw {
+                        marked = true
+                    }
+                })
             })
-            .collect::<Vec<_>>();
-
-        let (mut win_indices, win_grids): (Vec<_>, Vec<_>) = winners.into_iter().unzip();
-
-        let points: Vec<i32> = win_grids
-            .iter()
-            .map(|winner| {
-                winner
-                    .iter()
-                    .filter(|&&cell| cell != "x")
-                    .map(|&x| x.parse::<i32>().unwrap())
-                    .fold(0, |a, b| a + b)
-            })
-            .map(|x| x * n.parse::<i32>().unwrap())
-            .collect();
-        scores.extend(points);
-
-        win_indices.sort_by(|a, b| b.cmp(a));
-
-        for idx in win_indices {
-            arr.remove_index(Axis(0), idx);
-        }
+        });
     }
-    scores
+
+    vec![0, 1]
+    // let num: Regex = Regex::new(r"\d+").unwrap();
+    // let flattened_boards: Vec<&str> = num.find_iter(boards).map(|x| x.as_str()).collect();
+
+    // let shape = (
+    //     &flattened_boards.len() / (GRID_SIZE * GRID_SIZE),
+    //     GRID_SIZE,
+    //     GRID_SIZE,
+    // );
+
+    // let mut arr = Array3::from_shape_vec(shape, flattened_boards).unwrap();
+    // let mut scores: Vec<i32> = Vec::new();
+
+    // for n in numbers {
+    //     arr.mapv_inplace(|a| if a == n { "x" } else { a });
+
+    //     let winners: Vec<(usize, Board)> = arr
+    //         .outer_iter()
+    //         .enumerate()
+    //         .filter(|(_, board)| check_rows(board) || check_columns(board))
+    //         .collect::<Vec<_>>();
+
+    //     let (mut win_indices, win_boards): (Vec<_>, Vec<_>) = winners.into_iter().unzip();
+
+    //     let points: Vec<i32> = win_boards
+    //         .iter()
+    //         .map(|winner| {
+    //             winner
+    //                 .iter()
+    //                 .filter(|&&cell| cell != "x")
+    //                 .map(|&x| x.parse::<i32>().unwrap())
+    //                 .fold(0, |a, b| a + b)
+    //         })
+    //         .map(|x| x * n.parse::<i32>().unwrap())
+    //         .collect();
+    //     scores.extend(points);
+
+    //     win_indices.sort_by(|a, b| b.cmp(a));
+
+    //     for idx in win_indices {
+    //         arr.remove_index(Axis(0), idx);
+    //     }
+    // }
+    // scores
 }
 
 fn main() {
