1334f06b4a1c6ef2b223e1c47b3dee1f02a987b1
Chris Wallace <chris@bitcollider.io>
2021-12-07 20:07:07 +0000

    Refactor day 07 for better naming

--- rust/day-07/src/main.rs
+++ rust/day-07/src/main.rs
@@ -1,15 +1,25 @@
 const INPUT: &str = include_str!("../data/input.txt");
 
-fn constant_rate_fuel(input: &Vec<i32>) -> i32 {
-    let median = input[input.len() / 2];
-    input.iter().map(|n| (n - median).abs()).sum()
+fn parse_positions(input: &str) -> Vec<i32> {
+    let mut positions: Vec<i32> = input
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+
+    positions.sort();
+    positions
 }
 
-fn crab_rate_fuel(input: &Vec<i32>) -> i32 {
-    let mean = input.iter().sum::<i32>() as f32 / input.len() as f32;
+fn constant_rate_fuel(positions: &Vec<i32>) -> i32 {
+    let median = positions[positions.len() / 2];
+    positions.iter().map(|n| (n - median).abs()).sum()
+}
+
+fn crab_rate_fuel(positions: &Vec<i32>) -> i32 {
+    let mean = positions.iter().sum::<i32>() as f32 / positions.len() as f32;
 
     let fuel = |mean: i32| {
-        input
+        positions
             .iter()
             .map(|n| {
                 let dist = (n - mean).abs();
@@ -22,43 +32,27 @@ fn crab_rate_fuel(input: &Vec<i32>) -> i32 {
 }
 
 fn main() {
-    let mut input: Vec<i32> = INPUT
-        .split(',')
-        .map(|n| n.parse::<i32>().unwrap())
-        .collect();
-
-    input.sort();
-
-    println!("Part one fuel: {:?}", constant_rate_fuel(&input));
-    println!("Part two fuel: {:?}", crab_rate_fuel(&input));
+    let positions = parse_positions(INPUT);
+    println!("Part one fuel: {:?}", constant_rate_fuel(&positions));
+    println!("Part two fuel: {:?}", crab_rate_fuel(&positions));
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::{constant_rate_fuel, crab_rate_fuel};
+    use super::{constant_rate_fuel, crab_rate_fuel, parse_positions};
 
     const SAMPLE: &str = include_str!("../data/test.txt");
 
     #[test]
     fn test_constant_rate_fuel() {
-        let mut input: Vec<i32> = SAMPLE
-            .split(',')
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect();
-
-        input.sort();
-        assert_eq!(constant_rate_fuel(&input), 37);
+        let positions = parse_positions(SAMPLE);
+        assert_eq!(constant_rate_fuel(&positions), 37);
     }
 
     #[test]
     fn test_crab_rate_fuel() {
-        let mut input: Vec<i32> = SAMPLE
-            .split(',')
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect();
-
-        input.sort();
-        assert_eq!(crab_rate_fuel(&input), 168);
+        let positions = parse_positions(SAMPLE);
+        assert_eq!(crab_rate_fuel(&positions), 168);
     }
 }
